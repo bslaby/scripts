e1f472d07a2b486060d6973e8ae577fa706f2c4b
Beate Slaby <beate.slaby@uni-wuerzburg.de>
2015-10-29 11:18:26 +0100

    Update avgcov_from_samtoolsout.py

--- avgcov_from_samtoolsout.py
+++ avgcov_from_samtoolsout.py
@@ -10,11 +10,11 @@ import sys, os
 # input files:
 
 def usage():
-	print "Usage: avgcov_from_samtoolsout.py samtoolsout with samtoolsout as the output of 'samtools depth'"
+    print "Usage: avgcov_from_samtoolsout.py samtoolsout with samtoolsout as the output of 'samtools depth'"
 
 if len(sys.argv) != 2:
-	usage()
-	exit()
+    usage()
+    exit()
 
 with open(sys.argv[1], "rU") as samtoolsout:
 
@@ -22,20 +22,19 @@ with open(sys.argv[1], "rU") as samtoolsout:
 ### readcountdict is a dictionary counting the bases for each contig
     sumdict = {}
     readcountdict = {}
-    
     for line in samtoolsout:
-	    contigname = line.split("\t")[0]
-	    coverage = line.split("\t")[2]
-	    if not sumdict.has_key(contigname):
-	        sumdict[contigname] = int(coverage)
-	        readcountdict[contigname] = 1
-	    else:
-	        sumdict[contigname] = sumdict[contigname] + int(coverage)
-	        readcountdict[contigname] = readcountdict[contigname] + 1
+        contigname = line.split("\t")[0]
+        coverage = line.split("\t")[2]
+        if not sumdict.has_key(contigname):
+            sumdict[contigname] = int(coverage)
+            readcountdict[contigname] = 1
+        else:
+            sumdict[contigname] = sumdict[contigname] + int(coverage)
+            readcountdict[contigname] = readcountdict[contigname] + 1
 	        
 ### looping through the contigs in sumdict, the mean coverage per contig is calculated and written to standard output
     print "contig \t mean.coverage"
     for contig in sumdict:
-	    if readcountdict[contig] != 0:
-	        thevalue = float(sumdict[contig])/float(readcountdict[contig])
-	        print contig + "\t" + str(thevalue)
+        if readcountdict[contig] != 0:
+            thevalue = float(sumdict[contig])/float(readcountdict[contig])
+            print contig + "\t" + str(thevalue)
