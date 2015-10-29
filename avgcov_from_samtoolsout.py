0e9aee8c16f159317e427ccb5504bf442ef45e76
C. Titus Brown <titus@idyll.org>
2015-10-29 06:33:44 -0700

    some suggestions

--- avgcov_from_samtoolsout.py
+++ avgcov_from_samtoolsout.py
@@ -23,18 +23,35 @@ with open(sys.argv[1], "rU") as samtoolsout:
     sumdict = {}
     readcountdict = {}
     for line in samtoolsout:
-        contigname = line.split("\t")[0]
-        coverage = line.split("\t")[2]
-        if not sumdict.has_key(contigname):
+        #contigname = line.split("\t")[0]
+        #coverage = line.split("\t")[2]
+        # this avoids running 'split' twice.
+        contigname, _, coverage, _ = line.split('\t', 3)
+
+        
+        #if not sumdict.has_key(contigname):
+        if not contigname in sumdict: # 'in' is the same as has_key
             sumdict[contigname] = int(coverage)
             readcountdict[contigname] = 1
         else:
             sumdict[contigname] = sumdict[contigname] + int(coverage)
             readcountdict[contigname] = readcountdict[contigname] + 1
-	        
+
+        # ...but you can replace the whole if/else thing with:
+        # sumdict[contigname] = sumdict.get(contigname, 0) + int(coverage)
+        # readcountdict[contigname] = readcountdict.get(contigname, 0) + 1
+        #
 ### looping through the contigs in sumdict, the mean coverage per contig is calculated and written to standard output
     print "contig \t mean.coverage"
     for contig in sumdict:
         if readcountdict[contig] != 0:
             thevalue = float(sumdict[contig])/float(readcountdict[contig])
-            print contig + "\t" + str(thevalue)
+            # print contig + "\t" + str(thevalue)
+            # here, you can take out the + and just do:
+            print contig,'\t',thevalue
+            # which is faster and avoids the explicit 'str'.
+            # "better" is something like this:
+            # import csv
+            #    fp = csv.writer(open(...), delimiter='\t')
+            #    fp = csv.writerow([contig,thevalue])
+            # and then use csv to read the file also.
